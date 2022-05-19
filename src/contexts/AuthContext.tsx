f491f7f58c32476366e254ca6396a2b30272bfd4
Luiz Henrique <luizhjramos@outlook.com>
2022-05-19 10:20:32 -0300

    feat(authentication): authentication context provider

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, ReactNode } from "react";
+
+type TSignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type TAuthContextData = {
+  signIn(credentials: TSignInCredentials): Promise<void>;
+  isAuthenticated: boolean;
+};
+
+type TAuthProviderProps = {
+  children: ReactNode;
+};
+
+const AuthContext = createContext({} as TAuthContextData);
+
+export const AuthProvider = ({ children }: TAuthProviderProps) => {
+  const isAuthenticated = false;
+
+  const signIn = async ({ email, password }: TSignInCredentials) => {
+    console.log(email, password);
+  };
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
